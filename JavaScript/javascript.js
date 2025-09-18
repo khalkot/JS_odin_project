6aee3efe3f8738dd83fe3a39bd17fdc42ec67012
Khalkot <72662630+khalkot@user.noreply.github.com>
2025-09-18 09:17:11 +0300

    completed variables and opertators section

---
+++ JavaScript/javascript.js
@@ -0,0 +1,51 @@
+let name="khaled";
+let surname="alkot";
+
+console.log(name);
+console.log(surname);
+
+let age=55;
+
+console.log(age);
+
+age=22;
+
+console.log(age);
+
+const pi=3.14;
+/* pi =10; */ /* this line would resault in an error because pi is a const variable that cant modified */
+console.log(pi);
+
+console.log((3+2) - 76 * (1 + 1));
+
+console.log(23 + 97);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a=25;
+console.log(a);
+
+let b=a*7;
+console.log(b);
+
+let max=57;
+const actual = max - 13;
+const percentage=actual/max;
+
+console.log(percentage);
+
+let admin,
+    name2;
+name2="john";
+admin = name2;
+alert(admin);
+
+let planet="earth",
+    user;
+
+alert((50 % 9) * 10);
+let num1 = 4;
+alert(num1++);
